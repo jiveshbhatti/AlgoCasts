555cd244bd8c3965cac9be68e49480ed112025eb
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2022-01-03 09:58:20 -0800

    updated chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,44 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
 
+    let chunked = []
+
+for (let element of array){
+
+    const last = chunked[chunked.length - 1]
+
+    console.log(element)
+    console.log(last)
+
+
+    if(!last || last.length === size ){
+
+        chunked.push([element])
+    }else{
+        last.push(element)
+    }
+
+
+}
+
+console.log(chunked)
+
+return chunked
+}
+
+
+chunk([1,2,3,4,5,6],2)
 module.exports = chunk;
+
+
+function chunk(array, size) {
+    return array.reduce((chunks, curr) => {
+      let len = chunks.length
+      if (len === 0 || chunks[len - 1].length === size) chunks.push([curr])
+      else chunks[len - 1].push(curr)
+      return chunks
+    }, [])
+   }
+   
