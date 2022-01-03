61059107040d02322b1aea3ac49d580275895ab3
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2022-01-03 13:36:51 -0800

    two solutions

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,31 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
 
+    let str1 = str.split(' ')
+    console.log(str1)
+    let result = ''
+
+    str1.map((char)=>{
+      let newStr = char[0].toUpperCase() + char.slice(1).toString()
+      result = result + ' ' + newStr
+    })
+   
+console.log(result.trim())
+
+return result.trim()
+    
+
+}
+capitalize('a short sentence')
 module.exports = capitalize;
+
+
+function cap3(str) {
+    return str.split(' ').map(word => {
+        return word.replace(word.charAt(0), word.charAt(0).toUpperCase())
+    }).join(' ');
+   }
+
+   cap3('a short sentence')
