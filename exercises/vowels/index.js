77ce465b581b9b2e2034796851cb1557449d48af
jiveshbhatti <85315081+jiveshbhatti@users.noreply.github.com>
2022-02-17 10:30:23 -0800

    solution vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,34 +7,40 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-// function vowels(str) {
+let vowel = ['a','e','i','o','u']
+
+ function vowels(str) {
   
-//   let count = 0;
+  let count = 0;
 
-//   let string = str.toLowerCase();
+  let string = str.toLowerCase();
 
-//   for (let char of string) {
-//     for (let el of vowels) {
-//       if (char === el) {
-//         count++;
-//       }
-//     }
-//   }
-
-//   console.log(count);
-//   return count;
-// }
-
-function vowels(str) {
-    let vowels = /[aeiou]/ig;
+  for (let char of string) {
     
-    let string = str.toLowerCase()
-    
-return ( string.match(vowels).length)
-    
+    for (let el of vowel) {
+       
+      if (char === el) {
+        count++;
+      }
+    }
+  }
+
+  console.log(count);
+  return count;
 }
 
-vowels("hey how are you");
+
+vowels('hello how are you')
+// function vowels(str) {
+//     let vowels = /[aeiou]/ig;
+    
+//     let string = str.toLowerCase()
+    
+// return ( string.match(vowels).length)
+    
+// }
+
+// vowels("hey how are you");
 
 module.exports = vowels;
 
